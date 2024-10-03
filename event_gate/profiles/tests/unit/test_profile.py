8c3df54358ca5072ee3a6fcf9cd0c584d4ab3465
Karim Ouazzou <karimouazzou@gmail.com>
2024-10-03 16:32:48 +0100

    update tests

--- event_gate/profiles/tests/unit/test_profile.py
+++ event_gate/profiles/tests/unit/test_profile.py
@@ -1,16 +1,19 @@
 from django.test import TestCase
 from profiles.models import Profile
+from django.contrib.auth.models import User
 from datetime import date
 
 
 class ProfileTestCase(TestCase):
     def setUp(self):
-        """Create a test profile"""
-        Profile.objects.create(
-            first_name="John",
-            last_name="Doe",
+        """Create a test profile and user"""
+        user = User.objects.create_user(
+            username='john_doe',
             email="john.doe@test.com",
-            password="Test1234!",
+            password="Test1234!"
+        )
+        Profile.objects.create(
+            user=user,
             birth_date="1990-01-01",
             gender="Male",
             profile_picture="profile_pictures/default.jpg",
@@ -20,12 +23,14 @@ class ProfileTestCase(TestCase):
 
     def test_profile_data(self):
         """Test profile data"""
-        profile = Profile.objects.get(email="john.doe@test.com")
-        self.assertEqual(profile.first_name, 'John')
-        self.assertEqual(profile.last_name, 'Doe')
-        self.assertEqual(profile.password, 'Test1234!')
+        profile = Profile.objects.get(user__email="john.doe@test.com")
+        self.assertEqual(profile.user.first_name, '')
+        self.assertEqual(profile.user.last_name, '')
+        self.assertTrue(profile.user.check_password('Test1234!'))
         self.assertEqual(profile.birth_date, date(1990, 1, 1))
         self.assertEqual(profile.gender, 'Male')
-        self.assertEqual(profile.profile_picture, 'profile_pictures/default.jpg')
+        self.assertEqual(
+            profile.profile_picture, 'profile_pictures/default.jpg'
+        )
         self.assertEqual(profile.phone_number, '1234567890')
         self.assertEqual(profile.bio, 'I am a test user')
