a11d4914a1000b1684f6de36f737852c0ee95af0
Karim Ouazzou <karimouazzou@gmail.com>
2024-11-13 01:01:38 +0100

    update lifetime

--- event_gate/event_gate/settings.py
+++ event_gate/event_gate/settings.py
@@ -179,7 +179,7 @@ REST_FRAMEWORK = {
     )
 }
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'BLACKLIST_AFTER_ROTATION': True,
 }
